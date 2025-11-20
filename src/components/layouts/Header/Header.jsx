da7cc05e48df83810a71b70a9feecdbc4bf028fb
MatinGafarov <merlivion@gmail.com>
2025-11-20 14:22:35 +0400

    responsive all

--- src/components/layouts/Header/Header.jsx
+++ src/components/layouts/Header/Header.jsx
@@ -11,32 +11,47 @@ import styles from "./Header.module.css";
 
 // React Router
 import { Link, useLocation } from "react-router";
+import { useState } from "react";
 
 export const Header = () => {
   const location = useLocation();
+  const [menuActive, setMenuActive] = useState(false);
 
   const handleNavbarClass = (path) => {
     const isActive = location.pathname === path;
     return `${styles["navbar__link"]} ${isActive ? styles["navbar__link--active"] : ""}`;
   };
 
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header className={styles.header}>
       <nav className={styles.navbar}>
-       <div className={styles["navbar__logo"]}>
-  <Link to="/" className={styles["logo__link"]}>FASCO</Link>
-</div>
+        {/* Logo */}
+        <div className={styles["navbar__logo"]}>
+          <Link to="/" className={styles["logo__link"]}>FASCO</Link>
+        </div>
 
-        <ul className={styles["navbar__menu"]}>
+        {/* Burger icon */}
+        <div className={styles.burger} onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        {/* Menu */}
+        <ul className={`${styles["navbar__menu"]} ${menuActive ? styles.active : ""}`}>
           <li className={styles["navbar__item"]}>
             <Link to="/" className={handleNavbarClass("/")}>Home</Link>
           </li>
-
           <li className={styles["navbar__item"]}>
             <Link to="/shop" className={handleNavbarClass("/shop")}>Shop</Link>
           </li>
         </ul>
 
+        {/* Action Icons */}
         <div className={styles.actions}>
           <SearchIcon />
           <ProfileIcon />
