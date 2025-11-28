d576aa2c00ca6208c8d92d0b42b1dee7c4e1838b
unknown <suleymanovfidan26@gmail.com>
2025-11-28 14:40:36 +0400

    Fidan branch updated

---
+++ src/components/icons/StarIcon.jsx
@@ -0,0 +1,14 @@
+
+export const StarIcon = () => {
+    return (<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_6_52)">
+            <path d="M15 5.79532L9.54964 5.43699L7.49701 0.280273L5.44439 5.43699L0 5.79532L4.1758 9.34101L2.80553 14.7196L7.49701 11.7541L12.1885 14.7196L10.8183 9.34101L15 5.79532Z" fill="black" />
+        </g>
+        <defs>
+            <clipPath id="clip0_6_52">
+                <rect width="15" height="15" fill="white" />
+            </clipPath>
+        </defs>
+    </svg>)
+
+}
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,2 +1,16 @@
 export * from "./SearchIcon";
 export * from "./ProfileIcon";
+export * from "./StarIcon";
+export * from "./EmptyIcon";
+export * from "./EyeIcon";
+export * from "./CompareIcon";
+export * from "./QuestionIcon";
+export * from "./ShareIcon";
+export * from "./CarIcon";
+export * from "./BoxIcon";
+
+
+
+
+
+
